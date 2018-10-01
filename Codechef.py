4910a3ca082c34af79e8fee1b327cee53d016948
Rohan Balot <rohanbalot@gmail.com>
2018-10-01 21:53:47 +0530

    Update Codechef.py

--- Codechef.py
+++ Codechef.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup  # the scrape data from Code Chef
 import urllib2  # to get and post requestd using url
 import mechanize  # your backend browser
 import os  # for arranging into folders and renaming files
-list1 = []  # Creating an empty list
+list1 = [] 
 list2 = []
 difficulty = 'school'  # enter your prefered level in place of school
 
